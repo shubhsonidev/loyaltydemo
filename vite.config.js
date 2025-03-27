78c946d1c0d08f8f06654743ffe6cb7b988b7ba3
Shubh soni <shubhsoni.dev@gmail.com>
2025-03-27 14:38:18 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/loyaltycare/", // Set base path similar to --base-href in Angular
+  base: "/loyaltycare/",
   plugins: [tailwindcss(), react()],
 });
